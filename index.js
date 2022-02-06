5f578ccd929fd2a08d541b055f6d0ca6d1887502
Philip Levy <philipdlevy@gmail.com>
2022-02-05 21:02:38 -0700

    commit tree d4f4feb1be9b5543bd836a6627e0c1bc03093e53

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+
+
+
+
+function distanceFromHqInBlocks(someValue) {
+    console.log(someValue)
+    const hq = 42
+    return Math.abs(hq - someValue);
+};
+
+function distanceFromHqInFeet(someValue) {
+    distanceFromHqInBlocks(someValue); 
+        return Math.abs(42 - someValue) * 264
+    
+};
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+};
+
+function calculatesFarePrice(start, destination) {
+   const numFeet = distanceTravelledInFeet(start, destination)
+    if (numFeet < 400) {
+        return 0
+    } else if (numFeet > 400 && numFeet < 2000) {
+        return (numFeet - 400) * .02
+    } else if (numFeet > 2000 && numFeet < 2500) {
+        return 25
+    } else {
+        return "cannot travel that far"
+    }
+};
+
+
+
